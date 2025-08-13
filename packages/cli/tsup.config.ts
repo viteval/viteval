1aa0a87a3c4e71bf02167367bb109e19efcba628
Zac Rosenbauer <zacrosenbauer@gmail.com>
2025-08-13 14:49:41 -0400

    💾 saving work

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -1,7 +1,13 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
 import { defineConfig } from 'tsup';
+
+const OUTPUT_DIR = path.join(import.meta.dirname, 'dist');
+const TEMPLATES_DIR = path.join(import.meta.dirname, 'src', 'templates');
 
 export default defineConfig({
   entry: ['src/index.ts'],
+  outDir: OUTPUT_DIR,
   format: ['esm'],
   dts: false,
   clean: true,
@@ -10,5 +16,15 @@ export default defineConfig({
   sourcemap: true,
   minify: true,
   external: ['vitest', 'find-up', 'yargs', 'consola'],
-  publicDir: 'src/templates',
+  async onSuccess() {
+    const files = await fs.readdir(TEMPLATES_DIR);
+    for (const file of files) {
+      if (file.endsWith('.ts') && !file.endsWith('index.ts')) {
+        await fs.copyFile(
+          path.join(TEMPLATES_DIR, file),
+          path.join(OUTPUT_DIR, file)
+        );
+      }
+    }
+  },
 });
