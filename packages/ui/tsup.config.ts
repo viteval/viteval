ae766b30bd483353b031175e3d99671d33ada1b7
Zac Rosenbauer <zacrosenbauer@gmail.com>
2025-09-08 21:06:36 -0400

    💾 saving work

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,6 +1,33 @@
-import fs from 'node:fs/promises';
-import path from 'node:path';
 import { defineConfig } from 'tsup';
+
+// try {
+//   // Clean up the dist directory
+//   const exists = await fs
+//     .access(path.join(import.meta.dirname, 'dist', '.output', 'node_modules'))
+//     .then(() => true)
+//     .catch(() => false);
+//   if (exists) {
+//     await fs.rm(
+//       path.join(import.meta.dirname, 'dist', '.output', 'node_modules'),
+//       {
+//         recursive: true,
+//         force: true,
+//       }
+//     );
+//   }
+
+//   await fs.cp(
+//     path.join(import.meta.dirname, '.output'),
+//     path.join(import.meta.dirname, 'dist', '.output'),
+//     {
+//       recursive: true,
+//       // dereference: true,
+//       verbatimSymlinks: true,
+//     }
+//   );
+// } finally {
+//   // do nothing
+// }
 
 export default defineConfig({
   entry: ['server.ts'],
@@ -13,24 +40,9 @@ export default defineConfig({
   outDir: 'dist',
   format: ['esm'],
   dts: true,
-  clean: true,
+  clean: false,
   target: 'esnext',
   splitting: false,
   sourcemap: true,
   minify: true,
-  async onSuccess() {
-    try {
-      await fs.cp(
-        path.join(import.meta.dirname, '.output'),
-        path.join(import.meta.dirname, 'dist', '.output'),
-        {
-          recursive: true,
-          // dereference: true,
-          verbatimSymlinks: true,
-        }
-      );
-    } finally {
-      // do nothing
-    }
-  },
 });
