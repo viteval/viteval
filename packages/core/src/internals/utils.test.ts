b840765eb436983a88a3e078844fe170ab2515ab
Zac Rosenbauer <zacrosenbauer@gmail.com>
2025-09-08 21:20:44 -0400

    💾 saving work

--- packages/core/src/internals/utils.test.ts
+++ packages/core/src/internals/utils.test.ts
@@ -2,11 +2,11 @@ import { describe, expect, it } from 'vitest';
 import { resolve } from './utils';
 
 describe('resolve', () => {
-  it('handles non-promise values', () => {
-    expect(resolve('test')).resolves.toBe('test');
+  it('handles non-promise values', async () => {
+    await expect(resolve('test')).resolves.toBe('test');
   });
 
   it('handles promise values', async () => {
-    expect(new Promise((resolve) => resolve('test'))).resolves.toBe('test');
+    await expect(new Promise((resolve) => resolve('test'))).resolves.toBe('test');
   });
 });
